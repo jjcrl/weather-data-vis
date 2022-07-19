237a08e9d00de51202a100e22393b0aac29c0b2e
jjcrl <j412cff@icloud.com>
2022-07-19 21:36:20 +0100

    fonts bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./fonts/GtMaru/GT-Maru-Regular-Trial.otf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
